98055460753edff92feae13202654b5a2de3ff18
antoine-manu <agaudry.lamanu@gmail.com>
2024-03-05 16:17:32 +0100

    Question 4 la vraie je crois que j'ai fais 4 et 5 en meme temps avant sorry

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,6 @@ type Person = {
 
 function getStatistics(): {meanAge: number, meanHeight: number} {
   const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
-  let meanAge:number = 0
-  let meanHeight:number = 0
-  persons.map(person => {
-    meanAge += person.age
-    meanHeight += person.height
-  })
-
-  return {meanAge: (meanAge/persons.length), meanHeight: (meanHeight/persons.length)}
 }
 
 function displayResult() {
