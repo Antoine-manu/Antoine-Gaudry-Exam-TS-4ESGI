ff86c1ea2268ad3b71f4868b3f7e1b8d8a9b9811
antoine-manu <agaudry.lamanu@gmail.com>
2024-03-05 16:17:47 +0100

    Question 5

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ type Person = {
 
 function getStatistics(): {meanAge: number, meanHeight: number} {
   const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
+  let meanAge:number = 0
+  let meanHeight:number = 0
+  persons.map(person => {
+    meanAge += person.age
+    meanHeight += person.height
+  })
+
+  return {meanAge: (meanAge/persons.length), meanHeight: (meanHeight/persons.length)}
 }
 
 function displayResult() {
