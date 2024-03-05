d68c1dd957832ec693fc4abc021aee71a97d1526
antoine-manu <agaudry.lamanu@gmail.com>
2024-03-05 16:11:11 +0100

    Question 3

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ type Person = {
 
 function getStatistics(): number {
   const persons: Person[] = JSON.parse(readFileSync("./persons.json").toString());
+  let oldestAge: number = 0
+  persons.map(person => {
+     if (person.age > oldestAge){
+       oldestAge = person.age
+     }
+  })
+  return oldestAge
 }
 
 function displayResult() {
